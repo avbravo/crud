ddfe8805668c68f8e2469ef4aa0445ee0b0a66af
avbravo <avbravo@avbravo>
2019-06-03 21:25:17 -0500

    subreporte corregido

--- src/main/java/com/avbravo/crud/controller/UsuarioController.java
+++ src/main/java/com/avbravo/crud/controller/UsuarioController.java
@@ -309,13 +309,10 @@ public class UsuarioController implements Serializable, IController {
             HashMap parameters = new HashMap();
             parameters.put("P_EMPRESA", "MI EMPRESA");
             
-            //
-//            String reportsDirPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reportes/usuario/");
-//            reportsDirPath += "/";
-//            parameters.put("SUBREPORT_DIR", reportsDirPath + "/rol_subreport2");
+            
             String reportsDirPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reportes/usuario");
             reportsDirPath += "/";
-            parameters.put("SUBREPORT_DIR", reportsDirPath + "rol_subreport");
+            parameters.put("SUBREPORT_DIR", reportsDirPath + "rol_subreport.jasper");
             printer.imprimir(list, ruta, parameters);
         } catch (Exception e) {
             errorServices.errorMessage(nameOfClass(),nameOfMethod(), e.getLocalizedMessage());
